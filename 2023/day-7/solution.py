5a5c504eb0bb414bc27105216b361162a0cbe6f6
8ana4nam1ni0n <24194029+8ana4nam1ni0n@users.noreply.github.com>
2024-07-08 17:14:43 +0000

    Added part 2 of day 7

--- 2023/day-7/solution.py
+++ 2023/day-7/solution.py
@@ -13,6 +13,7 @@ class Strength:
 
 
 CardStrength = {k:i+1 for i, k in enumerate("23456789TJQKA")}
+CardStrengthWithWildcard = {k:i+1 for i, k in enumerate("J23456789TQKA")}
 
 
 @dataclass
@@ -31,30 +32,66 @@ class Hand:
             case [2, 1, 1, 1]: return Strength.OnePair
             case _: return Strength.HighCard
 
-    def compare(self, other_hand: "Hand") -> int:
-        if self.strength() > other_hand.strength():
-            return 1
-        if self.strength() < other_hand.strength():
-            return -1
+    def strength_with_wildcard(self) -> int:
+        strength = Counter(self.cards)
+        wild_cards_count = strength.pop('J', 0)
+        match (sorted(strength.values(), reverse=True), wild_cards_count):
+            case ([5], _) | ([4], 1) | ([3], 2) | ([2], 3) | ([1], 4) |([], 5):
+                return Strength.FiveOfKind
+            case ([4, 1], _) | ([3, 1], 1) | ([2, 1], 2) | ([1 , 1], 3):
+                return Strength.FourOfKind
+            case ([3, 2], _) | ([2, 2], 1):
+                return Strength.FullHouse
+            case ([3, 1, 1], _) | ([2, 1, 1], 1) | ([1, 1, 1], 2):
+                return Strength.ThreeOfKind
+            case ([2, 2, 1], _):
+                return Strength.TwoPair
+            case ([2, 1, 1, 1], _) | ([1,1,1,1], 1):
+                return Strength.OnePair
+            case _:
+                return Strength.HighCard
 
-        for i in range(len(self.cards)):
-            us = CardStrength[self.cards[i]]
-            them = CardStrength[other_hand.cards[i]]
-            if us > them:
-                return 1
-            if us < them:
-                return -1
-        # this should never happen?
-        return 0
+
+
+    def compare(self, other_hand: "Hand") -> int:
+
+        if self.strength() == other_hand.strength():
+            for i in range(len(self.cards)):
+                us = CardStrength[self.cards[i]]
+                them = CardStrength[other_hand.cards[i]]
+                if us != them:
+                    return us - them
+
+        return self.strength() - other_hand.strength()
+
+    def compare_with_wildcard(self, other_hand: "Hand") -> int:
+
+        if self.strength_with_wildcard() == other_hand.strength_with_wildcard():
+            for i in range(len(self.cards)):
+                us = CardStrengthWithWildcard[self.cards[i]]
+                them = CardStrengthWithWildcard[other_hand.cards[i]]
+                if us != them:
+                    return us - them
+
+        return self.strength_with_wildcard() - other_hand.strength_with_wildcard()
+
+    # def __gt__(self, other) -> bool:
+    #     return self.compare(other) > 0
+    #
+    # def __lt__(self, other) -> bool:
+    #     return self.compare(other) < 0
+    #
+    # def __eq__(self, other) -> bool:
+    #     return self.compare(other) == 0
 
     def __gt__(self, other) -> bool:
-        return self.compare(other) == 1
+        return self.compare_with_wildcard(other) > 0
 
     def __lt__(self, other) -> bool:
-        return self.compare(other) == -1
+        return self.compare_with_wildcard(other) < 0
 
     def __eq__(self, other) -> bool:
-        return self.compare(other) == 0
+        return self.compare_with_wildcard(other) == 0
 
 
 def parse_input(data: list[str]) -> list:
@@ -78,6 +115,10 @@ if __name__ == "__main__":
         data = parse_input(f.read().splitlines())
 
 
+    # solution 1
+    # print(get_hand_ranks(data))
+
+    #solution 2
     print(get_hand_ranks(data))
 
 
