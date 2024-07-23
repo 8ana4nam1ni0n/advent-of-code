dace64fa8cc198f9a75f69ec7d049c991292bacd
Yoshuam Alicea <sharp@yoshuams-mbp.lan>
2024-07-23 02:15:28 -0400

    Added more logic part 16 still in progress

--- 2023/day-16/solution.py
+++ 2023/day-16/solution.py
@@ -26,41 +26,58 @@ MIRROR = {
 def get_split(tile: str, row, col) -> tuple[tuple[int, int], tuple[int, int]] | None:
     old_direction = (row, col)
 
-    if (tile == '|') and (old_direction == DIRECTION['e']) or (old_direction == DIRECTION['w']):
+    if (tile == '|') and (old_direction == DIRECTION['e'] or old_direction == DIRECTION['w']):
         return DIRECTION['n'], DIRECTION['s']
-    if (tile == '-') and (old_direction == DIRECTION['n']) or (old_direction == DIRECTION['s']):
+    if (tile == '-') and (old_direction == DIRECTION['n'] or old_direction == DIRECTION['s']):
         return DIRECTION['e'], DIRECTION['w']
     return None
+
+def paint_energized_tiles(visited: set, grid: Grid) -> None:
+    energized = []
+    for i in range(len(grid)):
+        row = []
+        for j in range(len(grid[0])):
+            if (i, j) in visited:
+                row.append("#")
+            else:
+                row.append(grid[i][j])
+        energized.append(row)
+    for r in energized:
+        print(''.join(r))
 
 
 def get_energized_tiles(grid: Grid) -> int:
     rows, cols = len(grid), len(grid[0])
-    tiles = 1
 
     q = deque([((0, 0), DIRECTION['e'])])
+    visited = {(0, 0)}
 
     while q:
-        print(f"Queue: {q}")
+        # print(f"Queue: {q}")
         (r, c), (dr, dc) = q.popleft()
-        nr, nc = r + dr, c + dc
 
-        # TODO need to mark nodes tiles as visited to prevent infinite loops
-        while (0 <= nr < rows) and (0 <= nc < cols):
-            tile = grid[nr][nc]
-            tiles += 1
-            print(f"{(r, c)}: tile: {tile}, energized: {tiles}")
+        while (0 <= r < rows) and (0 <= c < cols):
+            tile = grid[r][c]
+            visited.add((r, c))
+            # print(f"{(r, c)}: tile: {tile}, energized: {len(visited)}")
+            # TODO: Handle case when you form a loop and infinitely repeats
             if (tile == '/') or (tile == '\\'):
                 key = (tile, (dr, dc))
                 dr, dc = MIRROR[key]
             elif (tile == '|') or (tile == '-'):
                 directions = get_split(tile, dr, dc)
+                # print(f"dir: {directions} old_dir: {(dr, dc)}")
                 if directions:
                     d1, d2 = directions
-                    q.append(((nr, nc), d1))
-                    q.append(((nr, nc), d2))
+                    if (r + d1[0], c + d1[1]) not in visited:
+                        q.append(((r, c), d1))
+                    if (r + d2[0], c + d2[1]) not in visited:
+                        q.append(((r, c), d2))
                     break
-            nr, nc = nr + dr, nc + dc
-    return tiles
+            r, c = r + dr, c + dc
+    paint_energized_tiles(visited, grid)
+    print(visited)
+    return len(visited)
 
 
 if __name__ == "__main__":
